853cff9287122d5becf0a922af0b462daa956d89
jsantander-cl <j.santanderph@gmail.com>
2026-06-05 10:25:00 -0400

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
+import copy from 'rollup-plugin-copy' // <-- Importa el plugin
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    copy({
+      targets: [
+        { src: 'data.json', dest: 'dist' } // <-- Le dice a Vite: "Copia data.json a la carpeta final dist"
+      ],
+      hook: 'writeBundle' // Hace que se copie justo al terminar la compilación
+    })
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  }
 })
